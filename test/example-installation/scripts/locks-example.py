6cf6a9c83256de4e5d62c8a747dbdd07e578301b
Andrew Johnson <blackandred@users.noreply.github.com>
2019-08-03 19:38:34 +0200

    Clean up

--- test/example-installation/scripts/locks-example.py
+++ test/example-installation/scripts/locks-example.py
@@ -1,5 +1,4 @@
 import sys
-import re
 from time import sleep
 
 """
@@ -10,18 +9,11 @@ from time import sleep
 sys.path.append('/opt/boautomate')
 
 from boautomate.boautomatelib.nodeexecutor import NodeExecutor
-from boautomate.boautomatelib.nodeexecutor.pipeline import info, sh
+from boautomate.boautomatelib.nodeexecutor.pipeline import info
 from datetime import timedelta
 
 
 class LocksExample(NodeExecutor):
-    # configurable settings
-    regexp: re.Pattern
-    separator: str
-    latest_stable_name: str
-
-    present_tags: list
-
     def main(self):
        with self.locks.lock('locks-example', mode=self.locks.MODE_EXIT, expires_in=timedelta(hours=2)):
            info('Hello, the pipeline is now locked')
